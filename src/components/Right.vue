5d7ca4a2f5ef2297bd148401fbb3c4c5091aadfa
abbott <1425218151@qq.com>
2019-12-04 22:25:37 +0800

    first commit

---
+++ src/components/Right.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-if="kk==1">
+            <GoodList :goodId="1"></GoodList>
+        </div>
+        <div v-else-if="kk==2">
+            <GoodList :goodId="2"></GoodList>
+        </div>
+        <div v-else-if="kk==3">
+            <GoodList :goodId="1"></GoodList>
+        </div>
+    </div>
+</template>
+<script type="text/javascript">
+import Msg from './msg.js'
+import GoodList from './goodsList.vue'
+export default {
+    data(){
+        return {
+            kk: 1
+        }
+    },
+    mounted:function(){
+        var _this = this
+        Msg.$on('val',function(m){
+            _this.kk = m;
+        })
+    },
+    components:{
+        GoodList
+    }
+}
+</script>
---
+++ src/components/goodsList.vue
@@ -0,0 +1,65 @@
+<template>
+    <div name="show">
+        <ul class="goodlist">
+            <li v-for="good in list" :key="good.goodName">
+                <img v-bind:src="good.img">
+                <p>{{good.goodName}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<style type="text/css">
+.goodlist li{
+    width: 200px;
+    height: 200px;
+    list-style: none;
+    float: left;
+    font-size: 9px;
+    color: red;
+    margin-bottom: 30px;
+}
+.goodlist img{
+    width: 180px;
+    height: 180px;
+}
+</style>
+<script type="text/javascript">
+export default {
+    name:"show",
+    data(){
+        var obj = this;
+        var url="";
+        if(obj.goodId == 1){
+            url="json/bjb.json";
+        }else if(obj.goodId == 2){
+            url="json/phone.json";
+        }
+        this.$http.get(url).then(function(res){
+            obj.list = res.data;
+        })
+        return{
+            list:[]
+        }
+    },
+    props:{
+        goodId:Number
+    },
+    watch:{
+        goodId(){
+            var obj = this;
+            var url="";
+            if(obj.goodId == 1){
+                url="json/bjb.json";
+            }else if(obj.goodId == 2){
+                url="json/phone.json";
+            }
+            this.$http.get(url).then(function(res){
+                obj.list = res.data;
+            })
+            return{
+                list:[]
+            }
+        }
+    }
+}
+</script>
